716574656a984d04b282d279dfac1c499563e328
thiagotobias <thiagotobias@gmail.com>
2021-05-29 17:45:05 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-gkddef
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-gkddef)
---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
+import { appRoutingModule } from './app.routing';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { JwtInterceptor } from './_helpers/jwt.interceptor';
+import { ErrorInterceptor } from './_helpers/error.interceptor';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    appRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    AppComponent,
+    HomeComponent,
+    LoginComponent
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+    // provider used to create fake backend
+    fakeBackendProvider
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
